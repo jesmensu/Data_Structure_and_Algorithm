0953fcfc5d2830b609e0404eed0b2ac763d07337
jesmensu <jesmensultana@gmail.com>
2024-03-17 00:26:07 +0530

    Modified

--- Arrays/03-max_subarray_sum.py
+++ Arrays/03-max_subarray_sum.py
@@ -19,3 +19,25 @@ for i in range(len(arr)):
         start = new_start
 print(max_sum_total)
 print(start, end)
+
+def largestContiguousSum(arr) -> int:
+		# add your logic here
+		start = 0
+		new_start = 0
+		end = 0
+		curr = 0
+		max_sum = arr[0]
+		curr_sum = 0
+		while curr<len(arr):
+			if curr_sum>max_sum:
+				max_sum = curr_sum
+				end = curr-1
+				start = new_start
+			if curr_sum<=0:
+				curr_sum = 0
+				new_start = curr
+			curr_sum = curr_sum + arr[curr]
+			curr = curr + 1
+		return max_sum, start, end
+
+print(largestContiguousSum(arr))
